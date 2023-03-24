6310db3ec2b1f796234bb85b71c205de6ae0569b
ReicaD <dbamanya447@gmail.com>
2023-03-24 01:34:39 -0400

    modified

--- github-finder/src/Pages/About.jsx
+++ github-finder/src/Pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h1 className="text-6xl mb-4">About DevTracker</h1>
-      <p className="mb-4 text-2xl font-light">
+      <p className="mb-4 text-xl font-dark">
         A React app to search GitHub profiles and see profile details. This
         project is part of the
         <a href="https://www.udemy.com/course/modern-react-front-to-back/">
@@ -17,10 +17,10 @@ function About() {
         </strong>
         .
       </p>
-      <p className="text-lg text-gray-400">
-        Version <span className="text-white">3.0.0</span>
+      <p className="text-lg text-dark-400">
+        Version <span className="text-white">:3.2.1</span>
       </p>
-      <p className="text-lg text-gray-400">
+      <p className="text-lg text--400">
         Layout By:
         <a className="text-white" href="https://twitter.com/hassibmoddasser">
           Rugwanad
--- github-finder/src/Pages/NotFound.jsx
+++ github-finder/src/Pages/NotFound.jsx
@@ -10,7 +10,7 @@ function NotFound() {
           <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
           <p className='text-5xl mb-8'>404 - Page Not Found!</p>
           <Link className='btn btn-primary btn-lg' to='/'>
-            <FaHome className='mr-2' />
+            <FaHome className='mr-0' />
             
           </Link>
         </div>
