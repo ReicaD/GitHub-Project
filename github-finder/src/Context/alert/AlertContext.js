8102ae96f943adb28cdd0c9632b9c164173814ba
ReicaD <dbamanya447@gmail.com>
2023-04-06 00:56:30 -0400

    setting alert

--- github-finder/src/Context/alert/AlertContext.js
+++ github-finder/src/Context/alert/AlertContext.js
@@ -3,13 +3,22 @@ import alertReducer from "./AlertReducer";
 
 const AlertContext = createContext();
 export const AlertProvider = ({ children }) => {
-  const initialState = null
-//set alert
+  const initialState = null;
 
   const [state, dispatch] = useReducer(alertReducer, initialState);
+  //setting the alert
+  const setAlert = () => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { msg, type },
+    });
+    setTimeout(() => dispatch({ type: "REMOVE_ALERT" }),4000);
+  };
 
-  return <AlertContext.Provider value={{alert:state}}>
-{children}
-  </AlertContext.Provider>
+  return (
+    <AlertContext.Provider value={{ alert: state }}>
+      {children}
+    </AlertContext.Provider>
+  );
 };
 export default AlertContext;
