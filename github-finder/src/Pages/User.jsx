d34df23834da7607d549a65cac751b532171a87a
ReicaD <dbamanya447@gmail.com>
2023-04-07 17:38:54 -0400

    Modified with classes

--- github-finder/src/Pages/User.jsx
+++ github-finder/src/Pages/User.jsx
@@ -4,19 +4,55 @@ import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Spinner from "../components/Layout/Spinner";
-
 import GithubContext from "../Context/github/GithubContext";
 
 function User() {
   const { getUser, user, loading } = useContext(GithubContext);
-const params = useParams()
+  const params = useParams();
   useEffect(() => {
     getUser(params.login);
   }, []);
+
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
   if (loading) {
     return <Spinner />;
   }
-  return <div>{user.login}</div>;
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back to Search
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 mb:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default User;
