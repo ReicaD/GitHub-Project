c68b1535d431f464b62b78b5abd23a31db7afa6e
ReicaD <dbamanya447@gmail.com>
2023-04-07 01:13:20 -0400

    Modified with User profile and spinner

--- github-finder/src/Pages/User.jsx
+++ github-finder/src/Pages/User.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import {useEffect, useContext} from "react"
-import GithubContext from '../Context/github/GithubContext'
+import React from "react";
+import { faCodepen, FaStore, FauserFriends, FaUsers } from "react-icons/fa";
+import { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Spinner from "../components/Layout/Spinner";
+
+import GithubContext from "../Context/github/GithubContext";
 
 function User() {
-  const {getUser,user }= useContext(GithubContext)
-
-  useEffect(()=>{
-     getUser()
-  },[])
-  return (
-
-    <div>USER</div>
-  )
+  const { getUser, user, loading } = useContext(GithubContext);
+const params = useParams()
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+  if (loading) {
+    return <Spinner />;
+  }
+  return <div>{user.login}</div>;
 }
 
-export default User
+export default User;
--- github-finder/src/components/Layout/Spinner.jsx
+++ github-finder/src/components/Layout/Spinner.jsx
@@ -1,16 +1,8 @@
-import Spinners from "./assets/Spinner-2.gif";
+import spinner from "./assets/Spinner-2.gif";
+import React, { Fragment } from 'react'
 
-function Spinner() {
-  return (
-    <div className="w-100 mt-20">
-      <img
-        width={180}
-        className="text-center mx-auto"
-        src="{spinner}"
-        alt="Loading..."
-      />
-    </div>
-  );
-}
-
+const Spinner = () => <Fragment>
+      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+    </Fragment>
 export default Spinner;
+
