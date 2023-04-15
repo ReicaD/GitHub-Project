f2a9e68e745e3a3f70f23dc4ed2a60c816db67ef
ReicaD <dbamanya447@gmail.com>
2023-04-14 23:48:42 -0400

    modified with bug fixers WD

--- github-finder/src/components/users/UserItems.jsx
+++ github-finder/src/components/users/UserItems.jsx
@@ -23,6 +23,6 @@ function UserItems({ user: { login, avatar_url } }) {
   );
 }
 UserItems.propTypes = {
-  repos: PropTypes.object.isRequired,
+  repos: PropTypes.object.isRequired
 };
 export default UserItems;
