ff9ba0e796941774fd22ba3fb4f6be7b7cf39df8
ReicaD <dbamanya447@gmail.com>
2023-04-17 21:15:22 -0400

    modified with axios.

--- github-finder/src/Context/github/GIthubActions.js
+++ github-finder/src/Context/github/GIthubActions.js
@@ -1,38 +1,48 @@
+import  axios from "axios"
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
+
+const github=axios.create({baseURL:GITHUB_URL})
 //setting the action for get search results
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {});
-  const { items } = await response.json();
+  //this will give us the response including the Json Data
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
+  // const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {});
+  // const { items } = await response.json();
 
-  return items;
+  // return items;
+
+  
 };
-//get single user
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`);
 
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-    return data;
-  }
-};
-//get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  });
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
 
-  const data = await response.json();
+// //get single user
+// export const getUser = async (login) => {
+//   const response = await fetch(`${GITHUB_URL}/users/${login}`);
 
-  return data;
-};
+//   if (response.status === 404) {
+//     window.location = "/notfound";
+//   } else {
+//     const data = await response.json();
+//     return data;
+//   }
+// };
+// //get user repos
+// export const getUserRepos = async (login) => {
+//   const params = new URLSearchParams({
+//     sort: "created",
+//     per_page: 10,
+//   });
+//   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
+
+//   const data = await response.json();
+
+//   return data;
+// };
 
 export const clearAllUsers = async (login) => {
   if (window.confirm("Are you sure you want to clear?")) {
